8d41d3ad1d9771c1b2817a49721eea5659d9d580
cechlauren <59663720+cechlauren@users.noreply.github.com>
2020-02-18 16:39:45 -0800

    comment and add test cases

--- test/test_optimization.py
+++ test/test_optimization.py
@@ -1,11 +1,73 @@
+import sys
+from BMI203_HW3_alignment.sequences import *
+from BMI203_HW3_alignment.optimizeMatrix import *
+from BMI203_HW3_alignment.matrices import *
 import numpy as np
 from smith_waterman import algs
 
-def test_roc():
+tpAlignments, tnAlignments = getAlignments(scoringMatrix=blosum50, gapStart=-10, gapExtend= -1)
+
+def test_get_alignments():
+    #assert tpAlignments[5] == ("RAECIQR-GVSPSQAQGLGSNLVTE", "RKRKIDRDAVLNMWQQGLGASHISK")
     return None
 
-def test_smithwaterman():
-    return None
 
+#will redo scoring from sw
 def test_scoring():
+    #testA, testB = tpAlignments[5]
+    #assert scoreAlignment(testA, testB, blosum50, -10, -1) ==35
     return None
+#no test of obj fxn code since lots of OH.
+
+#make sure matrix changes but wont make more copies than necessary
+def test_mutate_matrix():
+    #M = blosum.50.copy()
+    #M2 = mutateMatrix(M, 1, 1)
+    #assert M2 = M
+    #assert M2 != blosum50
+    return None
+
+def test_selection():
+	# Very random and hard to test, but I want to make sure that my weights
+	# are working and that I'm sampling with replacement: 
+	#L = ["a", "b", "c"]
+	#w = [0, 0, 1]
+	#assert selection(L, w) == ["c", "c", "c"]
+
+def test_scale_scores():
+	# just make sure I'm doing what I said: largest/smallest is 10^selectivePressure
+	# and the sum is 1.
+	#a,b = scaleScores([1, 2], 1)
+	#assert a+b == 1
+	#assert b/a == 10
+
+# For the actual meat of the function, I mostly can only use the running output to
+# verify that it's running correctly. Here, I make sure that all our data stays
+# the right length:
+def test_geneticAlg_skeleton():
+	#pop, scores, library, objectiveMeans = optimizeMatrix_geneticAlg(
+		blosum50, 1, 1, 1, 10, 5, 5, 3, -10, -1, tpAlignments, tnAlignments)
+	#assert len(pop) == 10
+	#assert len(scores) == 10
+	#assert len(library) == 3
+	#assert len(objectiveMeans) == 5 + 1 # there's an extra -inf at the beginning so that it
+										# starts off increasing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
