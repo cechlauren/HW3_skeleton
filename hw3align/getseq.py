65e63af345c927d129ebd0cbbe25ccee300bba59
cechlauren <59663720+cechlauren@users.noreply.github.com>
2020-02-18 21:11:23 -0800

    Update getseq.py

--- hw3align/getseq.py
+++ hw3align/getseq.py
@@ -19,8 +19,8 @@ for line in open("hw3align/seqnamesprep.txt", "r"):
 sequences = {}
 for filename in seqnames:
 	sequences[filename] = readFasta("hw3align/"+filename)
-positives = [line.rstrip().split() for line in open("hw3align/Pospairs.txt")]
-positives = [(sequences[fileA], sequences[fileB]) for fileA, fileB in positives]
+positivesfiles = [line.rstrip().split() for line in open("hw3align/Pospairs.txt")]
+positives = [(sequences[fileA], sequences[fileB]) for fileA, fileB in positivesfiles]
 
-negatives = [line.rstrip().split() for line in open("hw3align/Negpairs.txt")]
-negatives = [(sequences[fileA], sequences[fileB]) for fileA, fileB in negatives]
+negativesfiles = [line.rstrip().split() for line in open("hw3align/Negpairs.txt")]
+negatives = [(sequences[fileA], sequences[fileB]) for fileA, fileB in negativesfiles]
