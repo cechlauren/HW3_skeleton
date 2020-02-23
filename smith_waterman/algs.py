c22c93926aa4cc0ed9289c39e59a3d2870c53f16
cechlauren <59663720+cechlauren@users.noreply.github.com>
2020-02-23 14:51:34 -0800

    Update algs.py

--- smith_waterman/algs.py
+++ smith_waterman/algs.py
@@ -1,7 +1,4 @@
 import numpy as np
-import pandas as pd
-#from .utils import AASeq
-#from .io import read_active_sites, read_active_site, write_clustering, write_mult_clusterings
 
 def readsequence():
     return None
