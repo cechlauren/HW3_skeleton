b7dae0a17349b1f96a25ac0a2c887a50b6afa922
cechlauren <59663720+cechlauren@users.noreply.github.com>
2020-02-23 12:12:36 -0800

    comment

--- hw3align/getmatrix.py
+++ hw3align/getmatrix.py
@@ -1,31 +1,48 @@
 import sys
 
-def readFasta(filename):
+def read_substitution_matrix(filename):
 	'''
-	This function will read in a sequence from a fasta file
-	and return a string.
+	This function will be able to read in an amino acid substitution matrix from a file
+	that we need to apply to our comparisons of sequences.
+	
+	This function outputs a symmetrical sub. mat. dictionary containing the value of the substitution score.
+	It is keyed by a frozenset of the substitutions, where keys for matches will be of length 1. 
 	'''
-	seq = ""
-	for line in open(filename, "r"):
-		if line[0] == ">":
+	matrix = {} #make a dictionary
+	readHeader = False 
+	header = [] #an array
+	rowi = 0 #start at zero
+	for line in open("BMI203_HW3_alignment/" + filename, "r"):
+		#We dont care about comments
+		if line[0] == "#":
 			continue
-		seq = seq + line.rstrip().upper()
-	return seq
+		#Read in the line and make sure to separate on any whitespace
+		fields = line.rstrip().split()
+		#Read in the header
+		if readHeader == False:
+			header = fields
+			colLabels = fields #column labels
+			readHeader = True #now its been read
+			continue
+		#Confirm the correct number of fields
+		assert len(fields) == len(header)
+		row = header[rowi]
+		#Move through the fields and add each matrix entry
+		for col, score in zip(header, fields):
+			key = frozenset((row, col))
+			if key not in matrix:
+				matrix[key] = int(score) #make sure we report integers
+			else:
+				assert matrix[key] == int(score)
+		#Move on to next row
+		rowi += 1
+	return matrix
 
-#Still need to get the sequence names so I have a separate file containing the location of the fasta files
-#as well as their names:
-seqnames = []
-for line in open("hw3align/seqnamesprep.txt", "r"):
-	seqnames.append(line.rstrip())
+blosum50 = read_substitution_matrix("BLOSUM50")
+blosum62 = read_substitution_matrix("BLOSUM62")
+matio = read_substitution_matrix("MATIO")
+pam100 = read_substitution_matrix("PAM100")
+pam250 = read_substitution_matrix("PAM250")
 
-#This output will take the sequence name that I grabbed above and attach the appropriate sequence string.
-sequences = {}
-for filename in seqnames:
-	sequences[filename] = readFasta("hw3align/"+filename)
-#This identifies the positive pairs (given in Pospairs.txt) and will make an array containing the positive pairs tuples
-positivesfiles = [line.rstrip().split() for line in open("hw3align/Pospairs.txt")]
-positives = [(sequences[fileA], sequences[fileB]) for fileA, fileB in positivesfiles]
-
-#As above but with negative pairs
-negativesfiles = [line.rstrip().split() for line in open("hw3align/Negpairs.txt")]
-negatives = [(sequences[fileA], sequences[fileB]) for fileA, fileB in negativesfiles]
+matrix_names = ["BLOSUM50", "BLOSUM62", "MATIO", "PAM100", "PAM250"]
+matrices = map(read_substitution_matrix, matrix_names)
