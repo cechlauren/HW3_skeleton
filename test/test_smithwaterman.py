4587f3caa2b8e24853116a9dbb4cca4bf9aa0694
cechlauren <59663720+cechlauren@users.noreply.github.com>
2020-02-23 17:38:18 -0800

    explicitly said test_sw_blahblahblah

--- test/test_smithwaterman.py
+++ test/test_smithwaterman.py
@@ -1,31 +1,31 @@
 from hw3align.sequences import *
 from hw3align.getmatrix import blosum50
 from hw3align.smithwaterman import sw
-
+#############################################################################################
 #As a first test, lets make sure that the trace and alignment work okay regardless of starting 
 #with a gap: 
 def test_sw_Gap_beginB():
 	assert sw("EFGACD", "ACD", blosum50, -3, -1)[3:] == ("ACD", "ACD") 
 	#recall that sw() takes two strings to align, a scoring matrix, and gap start/extend penalties
-def sw_Gap_beginA_test():
+def test_sw_Gap_beginA():
 	assert sw("ACD", "EFGACD", blosum50, -3, -1)[3:] == ("ACD", "ACD")
 	#this is like above but now seq a has the gap
 #############################################################################################
 #As a second test, lets make sure that the trace and alignment work okay regardless of ending
 #with a gap: 
-#def sw_Gap_endA_test():
+def test_sw_Gap_endA():
 	#assert sw("ACD", "ACDEFG", blosum50, -3, -1)[3:] == ("ACD", "ACD")
-#def sw_Gap_endB_test():
+def test_sw_Gap_endB():
 	#assert sw("ACDEFG", "ACD", blosum50, -3, -1)[3:] == ("ACD", "ACD")
 #############################################################################################
 #As a third test, lets make sure sw() can trace thru matches & mismatches:
-#def sw_trace_mismatch_test():
-	#assert sw("ACDAFG", "ACDEFG", blosum50, -3, -1)[2:] == (41.0, 'ACDAFG', 'ACDEFG') #make sure the score is right
+def test_sw_trace_mismatch():
+	assert sw("ACDAFG", "ACDEFG", blosum50, -3, -1)[2:] == (41.0, 'ACDAFG', 'ACDEFG') #make sure the score is right
 #############################################################################################	
 #As a final test, make sure sw() can trace through gaps and extensions in both strings:
-#def sw_trace_indelB_test():
-	#assert sw("LAREN", "LARN", blosum50, -3, -1)[3:] == ('LAREN', 'LAR-N')
-#def sw_trace_indelA_test():
-	#assert sw("LARN", "LAREN", blosum50, -3, -1)[3:] == ('LAR-N', 'LAREN')
+def test_sw_trace_indelB():
+	assert sw("LAREN", "LARN", blosum50, -3, -1)[3:] == ('LAREN', 'LAR-N')
+def test_sw_trace_indelA():
+	assert sw("LARN", "LAREN", blosum50, -3, -1)[3:] == ('LAR-N', 'LAREN')
 
 
