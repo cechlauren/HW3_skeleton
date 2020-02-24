3f71c766e52dfc383eae215367f9d4a844e560ae
cechlauren <59663720+cechlauren@users.noreply.github.com>
2020-02-23 20:17:52 -0800

    Improve def test_geneticAlg

--- test/test_optimization.py
+++ test/test_optimization.py
@@ -207,7 +207,7 @@ def test_geneticAlg():
 	
 	pop, scores, library, objectiveMeans = optimizeMatrix_geneticAlg(
 		blosum50, 1, 1, 1, 10, 5, 5, 3, -10, -1, true_pos_align, true_neg_align)
-	return None
+	
 	assert len(pop) == 10
 	assert len(scores) == 10
 	assert len(library) == 3
