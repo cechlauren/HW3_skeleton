727c72836b2fd9184c8cc24d04f6427267105c0a
cechlauren <59663720+cechlauren@users.noreply.github.com>
2020-02-23 16:18:51 -0800

    checking input files

--- test/test_optimization.py
+++ test/test_optimization.py
@@ -1,45 +1,43 @@
 import sys
-from BMI203_HW3_alignment.sequences import *
-from BMI203_HW3_alignment.optimizeMatrix import *
-from BMI203_HW3_alignment.matrices import *
-import numpy as np
-from smith_waterman import algs
+from hw3align.sequences import *
+from hw3align.optimalmatrix import *
+from hw3align.getmatrix import blosum50
 
-tpAlignments, tnAlignments = getAlignments(scoringMatrix=blosum50, gapStart=-10, gapExtend= -1)
+#tpAlignments, tnAlignments = getAlignments(scoringMatrix=blosum50, gapStart=-10, gapExtend= -1)
 
-def test_get_alignments():
-	return None
+#def test_get_alignments():
+	#return None
     #assert tpAlignments[5] == ("RAECIQR-GVSPSQAQGLGSNLVTE", "RKRKIDRDAVLNMWQQGLGASHISK")
    
 
 
 #will redo scoring from sw
-def test_scoring():
-	return None
+#def test_scoring():
+	#return None
     #testA, testB = tpAlignments[5]
     #assert scoreAlignment(testA, testB, blosum50, -10, -1) ==35
     
 #no test of obj fxn code since lots of OH.
 
 #make sure matrix changes but wont make more copies than necessary
-def test_mutate_matrix():
-	return None
+#def test_mutate_matrix():
+	#return None
     #M = blosum.50.copy()
     #M2 = mutateMatrix(M, 1, 1)
     #assert M2 = M
     #assert M2 != blosum50
     
 
-def test_selection():
-	return None
+#def test_selection():
+	#return None
 	# Very random and hard to test, but I want to make sure that my weights
 	# are working and that I'm sampling with replacement: 
 	#L = ["a", "b", "c"]
 	#w = [0, 0, 1]
 	#assert selection(L, w) == ["c", "c", "c"]
 
-def test_scale_scores():
-	return None
+#def test_scale_scores():
+	#return None
 	
 	# just make sure I'm doing what I said: largest/smallest is 10^selectivePressure
 	# and the sum is 1.
@@ -51,8 +49,8 @@ def test_scale_scores():
 # For the actual meat of the function, I mostly can only use the running output to
 # verify that it's running correctly. Here, I make sure that all our data stays
 # the right length:
-def test_geneticAlg_skeleton():
-	return None
+#def test_geneticAlg_skeleton():
+	#return None
 	#pop, scores, library, objectiveMeans = optimizeMatrix_geneticAlg(
 		#blosum50, 1, 1, 1, 10, 5, 5, 3, -10, -1, tpAlignments, tnAlignments)
 	#assert len(pop) == 10
