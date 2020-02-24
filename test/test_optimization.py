ad1240d84c4aec387653efa61ab99a18bb9a880e
cechlauren <59663720+cechlauren@users.noreply.github.com>
2020-02-23 19:29:41 -0800

    test genetic alg

--- test/test_optimization.py
+++ test/test_optimization.py
@@ -19,10 +19,30 @@ def test_scoring():
     testA, testB = TP_Alignments[7]
     assert scoreAlignment(testA, testB, blosum50, -10, -1) == 35.0
 #making sure that the scoring example in my sw testing looks as expected
-#######################################################################################################    
-#no test of obj fxn code since lots of OH.
+#######################################################################################################
+#Utilize running output to verify that actual function is running correctly.
+#Confirm data stays the right length:
 
-#make sure matrix changes but wont make more copies than necessary
+def test_geneticAlg():
+	#return None
+	pop, scores, library, objectiveMeans = optimizeMatrix_geneticAlg(
+		blosum50, 1, 1, 1, 10, 5, 5, 3, -10, -1, TP_Alignments, TN_Alignments)
+	assert len(pop) == 10
+	#assert len(scores) == 10
+	#assert len(library) == 3
+	#assert len(objectiveMeans) == 5 + 1 # there's an extra -inf at the beginning so that it starts off increasing
+
+#######################################################################################################
+#If time, will test objective function:
+#true_pos_aligns, true_neg_aligns = getAlignments(scorematrix=blosum50, gap_start=-10, gap_extend=-1)
+#def test_objective_function():
+	#recall Obj. fxn: def scoreMatrix(true_pos_align, true_neg_align, scorematrix, gap_start, gap_extend):
+	#true_pos_align = true_pos_aligns[7, ?, ?]
+	#true_neg_align = true_neg_aligns[?, ?, ?]
+	#assert scoreMatrix(true_pos_align, true_neg_align, blosum50, -10, -1) == XXX
+
+#######################################################################################################	
+#Also important to check that the matrix changes but will not make excessive # of copies
 #def test_mutate_matrix():
 	#return None
     #M = blosum.50.copy()
@@ -49,18 +69,7 @@ def test_scoring():
 	#assert b/a == 10
 	
 
-# For the actual meat of the function, I mostly can only use the running output to
-# verify that it's running correctly. Here, I make sure that all our data stays
-# the right length:
-#def test_geneticAlg_skeleton():
-	#return None
-	#pop, scores, library, objectiveMeans = optimizeMatrix_geneticAlg(
-		#blosum50, 1, 1, 1, 10, 5, 5, 3, -10, -1, TP_Alignments, TN_Alignments)
-	#assert len(pop) == 10
-	#assert len(scores) == 10
-	#assert len(library) == 3
-	#assert len(objectiveMeans) == 5 + 1 # there's an extra -inf at the beginning so that it
-										# starts off increasing
+
 		
 
 
