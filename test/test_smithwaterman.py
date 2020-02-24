617512daf386a80531d50d18cbc9717a9387dc1f
cechlauren <59663720+cechlauren@users.noreply.github.com>
2020-02-23 17:18:06 -0800

    test sw for indel tracing

--- test/test_smithwaterman.py
+++ test/test_smithwaterman.py
@@ -20,24 +20,12 @@ def sw_Gap_endB_test():
 #############################################################################################
 #As a third test, lets make sure sw() can trace thru matches & mismatches:
 def sw_trace_mismatch_test():
-	assert sw("ACDAFG", "ACDEFG", blosum50, -3, -1)[2:] == (41.0, 'ACDAFG', 'ACDEFG')
-# I want to make sure I can trace through gaps and extensions in both strings:
-#def test_SW_trace_through_indel_B():
-	#assert smithWaterman("GARRETT", "GAETT", blosum50, -3, -1)[3:] == ('GARRETT', 'GA--ETT')
-#def test_SW_trace_through_indel_A():
-	#assert smithWaterman("GAETT", "GARRETT", blosum50, -3, -1)[3:] == ('GA--ETT', 'GARRETT')
+	assert sw("ACDAFG", "ACDEFG", blosum50, -3, -1)[2:] == (41.0, 'ACDAFG', 'ACDEFG') #make sure the score is right
+#############################################################################################	
+#As a final test, make sure sw() can trace through gaps and extensions in both strings:
+def sw_trace_indelB_test():
+	assert sw("LARENCECH", "LARNCECH", blosum50, -3, -1)[3:] == ('LARENCECH', 'LAR-NCECH')
+def sw_trace_indelA_test():
+	assert sw("LARNCECH", "LARENCECH", blosum50, -3, -1)[3:] == ('LAR-NCECH', 'LARENCECH')
 
-# I pulled a random pair and ran it through EMBOSS
-# to get the optimal alignment to check.
-# If it sticks around a while, the results should be at
-# https://www.ebi.ac.uk/Tools/services/web/toolresult.ebi?jobId=emboss_water-I20190223-011138-0845-38663360-p1m
-#testA, testB = posPairs[5]
-# This pair are the sequences 
-#('RFKWGPASQQILFQAYERQKNPSKEERETLVEECNRAECIQRGVSPSQAQGLGSNLVTEVRVYNWFANRRKEEAFRH', 'GRKRKIDRDAVLNMWQQGLGASHISKTMNIARSTVYKVINESN')
-#def test_SW_realData():
-	#start, end, bestScore, alignA, alignB = smithWaterman(testA, testB, blosum50, -10, -1)
-	#assert start == (35, 1)
-	#assert end == (59, 26)
-	#assert bestScore == 35
-	#assert alignA == "RAECIQR-GVSPSQAQGLGSNLVTE"
-	#assert alignB == "RKRKIDRDAVLNMWQQGLGASHISK"
+
